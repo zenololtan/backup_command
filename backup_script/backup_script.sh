78000e3ece7eb453dedb124f078cb9dbe076f031
Zeno Tan <ztan@f0r2s12.codam.nl>
2019-11-13 22:03:20 +0100

    added tar file. Now makes a date dir first and then a time dir with the new backup

--- backup_script/backup_script.sh
+++ backup_script/backup_script.sh
@@ -6,7 +6,7 @@
 #    By: ztan <ztan@student.codam.nl>                 +#+                      #
 #                                                    +#+                       #
 #    Created: 2019/11/13 13:54:36 by ztan           #+#    #+#                 #
-#    Updated: 2019/11/13 17:58:51 by ztan          ########   odam.nl          #
+#    Updated: 2019/11/13 21:57:57 by ztan          ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,26 +14,34 @@
 
 function Dircheck()
 {
-	string=`pwd`
-	if [[ $string != *$DIRNAME* ]]
+	BASE=`pwd`
+	if [[ "$BASE" != *"$DIRNAME"* ]]
 	then
 		echo "Error not in a project directory."
-		echo "Please put your project file in $DIRNAME or move to your project directory."
+		echo "Please put your project file in "$DIRNAME" or move to your project directory."
 		exit
 	fi
 }
 
 function Prod_name()
 {
-	BASE=`pwd`
 	P_NAME=`basename "$(dirname "$BASE")"`
-	while [ "$P_NAME" != $DIRNAME ]
+	while [ "$P_NAME" != "$DIRNAME" ]
 	do
 		cd ..
 		P_NAME=`basename "$(dirname $(pwd))"`
 	done
 	P_NAME=`basename "$(basename $(pwd))"`
 	SORC=`pwd`
+}
+
+function If_tar()
+{
+	EX="tar"
+	if [ "$ARG" == "$EX" ]
+	then
+		tar -zvcf file.tar.gz "$SORC"
+	fi
 }
 
 function Bfile_check()
@@ -47,45 +55,64 @@ function Bfile_check()
 
 function Prod_check()
 {
-	if [ ! -d $P_NAME ]
+	if [ ! -d "$P_NAME" ]
 	then
-		echo "Making $P_NAME directory."
-		mkdir $P_NAME
+		echo "Making "$P_NAME" directory."
+		mkdir "$P_NAME"
 	fi
 }
 
-function backup_n_return()
+function Dirmakemove()
 {
 	echo "Making backup."
-	DIR="$(date +"%Y-%b\%d-%H_%M")"
-	mkdir $DIR
-	cd $DIR
+	DIR="$(date +"%Y %b_%d")"
+	if [ ! -d "$DIR" ]
+	then
+		echo "Making "$DIR" directory."
+		mkdir "$DIR"
+	fi
+	cd "$DIR"
+	DIR="$(date +"%H_%M")"
+	mkdir "$DIR"
+	cd "$DIR"
+}
+
+function Backup_n_return()
+{
 	DEST=`pwd`
-	cp -a $SORC $DEST
+	echo "$SORC"
+	if [[ "$ARG" = "$EX" ]]
+	then
+		mv "$SORC/file.tar.gz" "$DEST"
+	else
+		cp -R "$SORC"/. "$DEST"
+	fi
 	echo "Backup finished."
 	echo "Backup path:"
-	echo $DEST
-	cd $BASE
+	echo "$DEST"
+	cd "$BASE"
 }
 
 function main()
 {
 	Dircheck
 	Prod_name
+	If_tar
 	cd ~/Desktop
 	Bfile_check
 	cd backup
 	Prod_check
 	cd $P_NAME
-	backup_n_return
+	Dirmakemove
+	Backup_n_return
 }
 
 # Customize the directory name to your projects.
 # The directory name is the name of the directroy where you put your project files.
 # If your projects are in the Desktop file, your directory name will be "Desktop".
-# DO NOT INSERT PATH NAME, ONLY INSERT THE NAME OF THE DIRECTORY WHERE YOUR PROJECTS ARE LOCATED.
 
 # Insert your directory name here
 DIRNAME="Desktop"
 cd $1
+ARG="$2"
 main
--- backup_script/custom_command.sh
+++ backup_script/custom_command.sh
@@ -6,8 +6,8 @@
 #    By: ztan <ztan@student.codam.nl>                 +#+                      #
 #                                                    +#+                       #
 #    Created: 2019/11/13 14:00:48 by ztan           #+#    #+#                 #
-#    Updated: 2019/11/13 14:34:32 by ztan          ########   odam.nl          #
+#    Updated: 2019/11/13 21:31:08 by ztan          ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
-alias backup='~/backup_command/backup_script/backup_script.sh $(pwd)'
+alias backup='~/backup_command/backup_script/backup_script.sh $(pwd) $2'
